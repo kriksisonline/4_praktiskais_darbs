4e76bee143ad8f461da05d0578fd4e204a1ff920
gustavkavac <gustavs.kavacs@gmail.com>
2022-11-08 22:49:32 +0200

    pashreizejais stavoklis

--- storage_management_system.cpp
+++ storage_management_system.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <string.h>
 
 #define FILE_NAME "the-file.txt"
 
 using namespace std;
 
 class Item {
-private:
+// private:
+//     char itemName[20];
+//     double itemPrice, itemQuantity, itemsSold;
+public:
     char itemName[20];
     double itemPrice, itemQuantity, itemsSold;
-public:
+
     void readItem() {
         cout << endl << "Informacija par produktu:" << endl;
         cout << "Nosaukums: ";
@@ -72,6 +76,8 @@ int readFromFile(string filename) {
     return 0;
 }
 
+int sell_product(string file_name);
+
 int main() {
     Item item;
     int menuItem;
@@ -79,9 +85,12 @@ int main() {
     cout << "0: Beigt darbibu" << endl;
     cout << "1: Pievienot produktu" << endl;
     cout << "2: Paradit visus produktus" << endl;
-    while (true) {
+    cout << "3: Pardot produktu" << endl;
+    while (true){
         cout << endl << "Izvele: ";
         cin >> menuItem;
+        cin.clear();
+        cin.ignore(1000, '\n');
         switch(menuItem) {
         case 0:
             return 0;
@@ -92,6 +101,9 @@ int main() {
         case 2:
             readFromFile(FILE_NAME);
             break;
+        case 3:
+            sell_product(FILE_NAME);
+            break;
         default:
             cout << "\nKluda! Nepareiza izvelne!" << endl;
             return 0;
@@ -100,4 +112,32 @@ int main() {
     return 0;
 }
 
-//testing:: adding a comment to the repo
+int sell_product(string file_name){
+    Item product;
+    Item sold_product;
+    fstream file;
+    char sold_name[20];
+    double quantity;
+    double sold;
+    
+    cout << "Kuru produktu pardod?" << endl;
+    cin >> sold_name;
+    cin.clear();
+    cin.ignore(1000, '\n');
+
+    file.open(file_name, ios::in|ios::binary);
+    while (true){
+        file.read((char*)&product, sizeof(product));
+        if (!strcmp(sold_name, product.itemName)){
+            return 0;
+        }
+        else if (file.eof()){
+            cout << "Kluda: produkta ievade!" << endl;
+            return 0;
+        }
+    }
+    quantity = product.itemQuantity;
+    sold = product.itemsSold;
+    file.close();
+    return 0;
+}
